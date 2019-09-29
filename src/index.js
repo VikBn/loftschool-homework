90bb0cefa580eecba2f85351baa4d92b2857925a
Viktor Bondarenko <32108055+VikBn@users.noreply.github.com>
2019-09-29 21:33:17 +0300

    homework_towns

--- src/index.js
+++ src/index.js
@@ -1,61 +1,98 @@
-/* ДЗ 6 - Асинхронность и работа с сетью */
-
 /*
- Задание 1:
-
- Функция должна возвращать Promise, который должен быть разрешен через указанное количество секунду
-
- Пример:
-   delayPromise(3) // вернет promise, который будет разрешен через 3 секунды
- */
-function delayPromise(seconds) {
-    return new Promise((resolve) => {
-        setTimeout(() => {
-            resolve();
-        }, seconds)
-    })
-}
-
-/*
- Задание 2:
-
- 2.1: Функция должна вернуть Promise, который должен быть разрешен с массивом городов в качестве значения
-
- Массив городов можно получить отправив асинхронный запрос по адресу
+ Страница должна предварительно загрузить список городов из
  https://raw.githubusercontent.com/smelukov/citiesTest/master/cities.json
+ и отсортировать в алфавитном порядке.
 
- 2.2: Элементы полученного массива должны быть отсортированы по имени города
+ При вводе в текстовое поле, под ним должен появляться список тех городов,
+ в названии которых, хотя бы частично, есть введенное значение.
+ Регистр символов учитываться не должен, то есть "Moscow" и "moscow" - одинаковые названия.
+
+ Во время загрузки городов, на странице должна быть надпись "Загрузка..."
+ После окончания загрузки городов, надпись исчезает и появляется текстовое поле.
+
+ Разметку смотрите в файле towns-content.hbs
+
+ Запрещено использовать сторонние библиотеки. Разрешено пользоваться только тем, что встроено в браузер
+
+ *** Часть со звездочкой ***
+ Если загрузка городов не удалась (например, отключился интернет или сервер вернул ошибку),
+ то необходимо показать надпись "Не удалось загрузить города" и кнопку "Повторить".
+ При клике на кнопку, процесс загрузки повторяется заново
+ */
+
+/*
+ homeworkContainer - это контейнер для всех ваших домашних заданий
+ Если вы создаете новые html-элементы и добавляете их на страницу, то добавляйте их только в этот контейнер
 
  Пример:
-   loadAndSortTowns().then(towns => console.log(towns)) // должна вывести в консоль отсортированный массив городов
+   const newDiv = document.createElement('div');
+   homeworkContainer.appendChild(newDiv);
  */
-function loadAndSortTowns() {
+const homeworkContainer = document.querySelector('#homework-container');
+
+/*
+ Функция должна вернуть Promise, который должен быть разрешен с массивом городов в качестве значения
+
+ Массив городов пожно получить отправив асинхронный запрос по адресу
+ https://raw.githubusercontent.com/smelukov/citiesTest/master/cities.json
+ */
+
+const loadTowns = () => {
     const url = 'https://raw.githubusercontent.com/smelukov/citiesTest/master/cities.json';
 
-    return new Promise((resolve) => {
-        const xhr = new XMLHttpRequest();
+    return fetch(url)
+        .then(res => res.json())
 
-        xhr.open('GET', url);
-        xhr.responseType = 'json';
-        xhr.addEventListener('load', () => {
+};
 
-            resolve(xhr.response.sort(function (a, b) {
-                if (a.name > b.name) {
-                    return 1;
-                }
-                if (a.name < b.name) {
-                    return -1;
-                }
+/*
+ Функция должна проверять встречается ли подстрока chunk в строке full
+ Проверка должна происходить без учета регистра символов
 
-                return 0;
-            }));
-        });
-        xhr.send()
-    })
+ Пример:
+   isMatching('Moscow', 'moscow') // true
+   isMatching('Moscow', 'mosc') // true
+   isMatching('Moscow', 'cow') // true
+   isMatching('Moscow', 'SCO') // true
+   isMatching('Moscow', 'Moscov') // false
+ */
+function isMatching(full, chunk) {
+    // let arr = chunk.split('').slice(1, chunk.length).join('');
+    // const ch = full[0] === chunk[0].toUpperCase() && full.includes(chunk[0].toUpperCase() + arr);
+    //
+    // return (full.includes(chunk) || full.includes(chunk.toLowerCase()) || ch);
+    return full.toLowerCase().includes(chunk.toLowerCase())
 }
 
-loadAndSortTowns();
+let cities = [];
+
+/* Блок с надписью "Загрузка" */
+const loadingBlock = homeworkContainer.querySelector('#loading-block');
+/* Блок с текстовым полем и результатом поиска */
+const filterBlock = homeworkContainer.querySelector('#filter-block');
+/* Текстовое поле для поиска по городам */
+const filterInput = homeworkContainer.querySelector('#filter-input');
+/* Блок с результатами поиска */
+const filterResult = homeworkContainer.querySelector('#filter-result');
+
+loadTowns()
+    .then(res => {
+        cities = res;
+        loadingBlock.classList.add('hidden');
+        filterBlock.classList.remove('hidden');
+    });
+
+filterInput.addEventListener('keyup', function () {
+    // это обработчик нажатия кливиш в текстовом поле
+    filterResult.innerHTML = filterInput.value
+        ? cities
+            .filter(item => isMatching(item.name, filterInput.value))
+            .map(item => `<div>${item.name}</div>`)
+            .join('')
+        : ''
+});
+
 export {
-    delayPromise,
-    loadAndSortTowns
-};
+    loadTowns,
+    isMatching
+ };
